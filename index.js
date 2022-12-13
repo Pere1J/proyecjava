221b50260ccdd979df2a27e7fe9fe30de0262d71
Pere1J <pere1josep@gmail.com>
2022-12-13 17:13:04 +0100

    copia de seguridad

--- index.js
+++ index.js
@@ -9,7 +9,26 @@ for (let i = 0; i < numberList.length; i++) {
    
    if(numberList[i] >= maxNumber) {maxNumber = numberList[i]}
 }
-console.log (maxNumber)
+console.log ('el número más alto es ' + maxNumber)
 
 //Dado un array de números imprime por consola la suma de todos los que sean pares.
 
+let numberList= [1,2,3,4,5,6,7,100,9]
+let pairNumbers= 0;
+
+for (let i = 0; i < numberList.length; i++) {
+   if (numberList[i] % 2 == 0) {pairNumbers += numberList[i];}
+   
+}
+console.log ('la suma de los pares es ' + pairNumbers);
+
+//Dado un array de números y strings imprime un array de solamente los strings.
+
+let itemsList=['gato', 1, 'perro', 2, 'pájaro', 3];
+
+console.log(itemsList)
+
+
+
+
+ 
