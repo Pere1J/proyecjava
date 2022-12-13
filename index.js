4dc6e91bfec8eab7a23bdf91bcc57b3869be01f9
Pere1J <pere1josep@gmail.com>
2022-12-13 16:00:01 +0100

    segundo ej

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-//datos tipos de datos
-console.log (5);
 
-//datos booleanos
-console.log (true);
+//Dado un array de números imprimir por consola el número mayor.
 
-let numberList= [1,2,3,4,5,6,7,8,9]
+
+let numberList= [1,2,3,4,5,6,7,100,9]
 let maxNumber=numberList [0];
 
 for (let i = 0; i < numberList.length; i++) {
@@ -12,3 +10,6 @@ for (let i = 0; i < numberList.length; i++) {
    if(numberList[i] >= maxNumber) {maxNumber = numberList[i]}
 }
 console.log (maxNumber)
+
+//Dado un array de números imprime por consola la suma de todos los que sean pares.
+
