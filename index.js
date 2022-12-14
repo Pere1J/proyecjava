39888cee3ddf090889a8bf379c7759aec2d2a846
Pere1J <pere1josep@gmail.com>
2022-12-14 08:54:32 +0100

    3r ej de javascript

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ console.log ('el número más alto es ' + maxNumber)
 
 //Dado un array de números imprime por consola la suma de todos los que sean pares.
 
-let numberList= [1,2,3,4,5,6,7,100,9]
+
 let pairNumbers= 0;
 
 for (let i = 0; i < numberList.length; i++) {
@@ -24,11 +24,9 @@ console.log ('la suma de los pares es ' + pairNumbers);
 
 //Dado un array de números y strings imprime un array de solamente los strings.
 
-let itemsList=['gato', 1, 'perro', 2, 'pájaro', 3];
+  let cats = ['Bill', 'Jeff', 'Pete', 'Biggles', 'Jasmin', 5, 6 , 7];
 
-console.log(itemsList)
-
-
-
-
- 
+for (let i = 0; i < cats.length; i++) {
+    if (typeof(cats[i])=="string") { 
+       console.log('mi gato se llama ' + cats[i]);}; 
+  }
